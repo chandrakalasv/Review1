4a03ca5c9abdfa05635e7c6da0f4a3f8b76a8faf
chandrakala <chadrakalasv1502@gmail.com>
2022-04-19 09:23:18 +0530

    [review]palindrome1

---
+++ palindrome1.sh
@@ -0,0 +1,10 @@
+1.User must first enter the value of the integer and store it in a variable.
+2. The value of the integer is then stored in another temporary variable.
+3. The while loop is used and the last digit of the number is obtained by using the modulus operator.
+4. The last digit is then stored at the one’s place, second last at the ten’s place and so on.
+5. The last digit is then removed by  dividing the number with 10.
+6. This loop terminates when the value of the number is 0.
+7. The reverse of the number is then compared with the integer value stored in the temporary variable.
+8. If both are equal, the number is a palindrome.
+9. If both aren’t equal, the number isn’t a palindrome.
+10. The final result is then printed.
